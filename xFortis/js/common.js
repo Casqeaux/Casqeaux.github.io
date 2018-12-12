a5ca4dd828309a686390295f6c254230470afbf4
CasqeauX <44642176+Casqeaux@users.noreply.github.com>
2018-12-12 14:27:40 +0300

    Revert "xFortis"
    This reverts commit d089518753d71d67bc3c6645c70d1972ccca563f.

--- xFortis/js/common.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-
-	// Custom JS
-	$(".box_mnu .panel-heading").each(function() {
-		var ph = $(this).height() +3;
-		var pdt = $(this).find(".dropdown-toggle");
-		pdt.height(ph);
-	});
-});
